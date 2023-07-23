d523df2b0f4deb9f0be34efdf8af0c64539fef24
Abhimanyu <91619054+Abhimanyu-jaiswal@users.noreply.github.com>
2023-07-23 11:29:05 +0000

    Added a file for add

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    a = 90;
+    b = 15;
+    c = a + b;
+    cout << "Addition of a and b is : " << c << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Demo-git
 This repo is created for learning the Git version control.
+
+# Addition of two numbers
+Gives the sum of two numbers.
